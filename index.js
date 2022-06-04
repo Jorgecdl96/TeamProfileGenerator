46a28a8117d783d953ee56f68b643dc2e3715ecb
Jorgecdl96 <100006802+Jorgecdl96@users.noreply.github.com>
2022-06-04 17:05:39 -0600

    creating html

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ const questionsIntern = [
     {
         type: 'input',
         message: 'What is the intern\'s School?',
-        name: 'github'
+        name: 'school'
     },
     {
         type: 'list',
@@ -93,22 +93,128 @@ const questionsIntern = [
     
 ];
 
+ 
+
+function htmlCreation(answers){
+
+const {managerName, managerEmail, managerID, managerOffice, role, engineerName, engineerEmail, engineerID, github, internName, internEmail, internID, school} = answers;
+
+if(managerName){
+    const managerContent = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+        <title>Document</title>
+    </head>
+    <body>
+        <header class="d-flex align-items-center justify-content-center" style="height: 5rem; background-color: rgb(240, 72, 21); color: white;;">
+            <h1>My Team</h1>
+        </header>
+    <div class="d-flex flex-wrap align-items-center justify-content-center">
+        <div class="card m-2" style="width: 18rem;" >
+            <div class="card-body">
+                <h4 class="card-title">${managerName}</h4>
+                <h5>Manager</h5>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID: ${managerID}</li>
+                <li class="list-group-item">Email: ${managerEmail}</li>
+                <li class="list-group-item">Office number: ${managerOffice}</li>
+              </ul> 
+        </div>
+    `
+
+    fs.writeFile('index2.html',managerContent, (err) => {
+        if(err){
+            console.log(err)}
+    })
+}
+
+if(engineerName){
+    const engineerContent = `<div class="card m-2" style="width: 18rem;" >
+    <div class="card-body">
+        <h4 class="card-title">${engineerName}</h4>
+        <h5>Engineer</h5>
+    </div>
+    <ul class="list-group list-group-flush">
+        <li class="list-group-item">ID: ${engineerID}</li>
+        <li class="list-group-item">Email: ${engineerEmail}</li>
+        <li class="list-group-item">Office number: ${github}</li>
+      </ul> 
+</div>
+`
+
+fs.appendFile('index2.html', engineerContent, (err) => {
+    if(err){
+        console.log(err);
+    }
+})
+
+}
+
+if(internName){
+    const internContent = `<div class="card m-2" style="width: 18rem;" >
+    <div class="card-body">
+        <h4 class="card-title">${internName}</h4>
+        <h5>Intern</h5>
+    </div>
+    <ul class="list-group list-group-flush">
+        <li class="list-group-item">ID: ${internID}</li>
+        <li class="list-group-item">Email: ${internEmail}</li>
+        <li class="list-group-item">Office number: ${school}</li>
+      </ul> 
+</div>
+`
+
+fs.appendFile('index2.html', internContent, (err) => {
+    if(err){
+        console.log(err);
+    }
+})
+
+}
+
+switch (role) {
+        case 'Engineer':
+            engineer.promptQuestions();
+            break;
+        case 'Intern':
+            intern.promptQuestions();
+            break;
+        default:
+        const finalContent = `</div>
+</body>
+</html>`  
+        fs.appendFile('index2.html',finalContent, (err) =>{
+        if (err) {
+            console.log(err);
+        }
+        })
+            break;
+        }
+        
+        
+        
+}
+
+
 class Questions {
     constructor(roleQuestions){
         this.roleQuestions = roleQuestions;
     }
-
+    
     promptQuestions(){
         inquirer
         .prompt(this.roleQuestions)
         .then((answers) => {
-
-            if (answers.role === 'Engineer') {
-                engineer.promptQuestions();
-            } else if(answers.role === 'Intern'){
-                intern.promptQuestions();
-            }
+            
+            htmlCreation(answers);
+            
         })
+        
     }
 }
 
@@ -116,8 +222,9 @@ const manager = new Questions(questionsManager);
 const engineer = new Questions(questionsEngineer);
 const intern = new Questions(questionsIntern);
 
+
 function init(){
-manager.promptQuestions();
+    manager.promptQuestions();
 }
 
 init();
